cca8dad9a2c537722857f447244a054933d2660e
pmendoza28 <joshanicetemendoza@gmail.com>
2021-12-03 10:06:57 +0800

    add admin-login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'administrator/authentication', loadChildren: () => import("./components/login/admin-login/admin-login.module").then(m => m.AdminLoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/login/admin-login/admin-login.module.ts
+++ src/app/components/login/admin-login/admin-login.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminLoginComponent } from './admin-login.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: AdminLoginComponent}
+]
 
 @NgModule({
   declarations: [
     AdminLoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AdminLoginModule { }
